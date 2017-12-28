7f569228f6b6f42bd6c91150877716c1d7cb1676
Nahuel Adrian Melo <nmelo@belatrix.com>
2017-12-28 15:48:40 -0300

    Automation login demotours.

--- Automation/GoogleSearch.py
+++ Automation/GoogleSearch.py
@@ -2,13 +2,13 @@ import time
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-driver = webdriver.Chrome("C:\\Users\\nmelo\\QATools\\Python\\PythonSelenium\\Automation\\Drivers\\chromedriver.exe")
-driver.get("http://www.google.com")
+driver = webdriver.Chrome("Drivers\\chromedriver.exe")
+driver.get('http://newtours.demoaut.com/')
 time.sleep(5)
-assert 'Google' in driver.title
-elem = driver.find_element_by_name("q")
-elem.send_keys("Pyton")
+elem = driver.find_element_by_name("userName")
+elem.send_keys("mercury")
+elem = driver.find_element_by_name("password")
+elem.send_keys("mercury")
 elem.send_keys(Keys.RETURN)
 driver.close()
 driver.quit()
-
